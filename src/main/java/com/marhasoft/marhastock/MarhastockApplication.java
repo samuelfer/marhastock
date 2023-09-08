d548441d7e55178674100861539e4c640840da82
sfsantos <sfsantos@tce.pb.gov.br>
2023-09-07 22:18:18 -0300

    Define estrutura de packages

---
+++ src/main/java/com/marhasoft/marhastock/MarhastockApplication.java
@@ -0,0 +1,13 @@
+package com.marhasoft.marhastock;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarhastockApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarhastockApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/marhasoft/marhastock/MarhastockApplicationTests.java
@@ -0,0 +1,13 @@
+package com.marhasoft.marhastock;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarhastockApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
