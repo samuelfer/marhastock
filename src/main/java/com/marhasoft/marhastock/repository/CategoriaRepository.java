b2fc6cd574d2b5071213d0771d401bfa684748b8
sfsantos <sfsantos@tce.pb.gov.br>
2023-09-08 21:29:07 -0300

    Add método para listar categoria pela descrição

--- src/main/java/com/marhasoft/marhastock/repository/CategoriaRepository.java
+++ src/main/java/com/marhasoft/marhastock/repository/CategoriaRepository.java
@@ -4,6 +4,10 @@ import com.marhasoft.marhastock.model.Categoria;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+
+    Optional<Categoria> findByDescricao(String descricao);
 }
