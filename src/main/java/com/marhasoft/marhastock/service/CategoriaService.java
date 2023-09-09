a24c6c70b32a3d25aa0a832a2b286c80f5ef1b79
sfsantos <sfsantos@tce.pb.gov.br>
2023-09-08 21:28:47 -0300

    Add modelMapp

--- src/main/java/com/marhasoft/marhastock/service/CategoriaService.java
+++ src/main/java/com/marhasoft/marhastock/service/CategoriaService.java
@@ -3,16 +3,20 @@ package com.marhasoft.marhastock.service;
 import com.marhasoft.marhastock.dto.CategoriaDTO;
 import com.marhasoft.marhastock.model.Categoria;
 import com.marhasoft.marhastock.repository.CategoriaRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CategoriaService {
 
     @Autowired
     private CategoriaRepository categoriaRepository;
+    @Autowired
+    private ModelMapper modelMapper;
 
     public List<Categoria> getAll() {
         return categoriaRepository.findAll();
@@ -24,10 +28,12 @@ public class CategoriaService {
     }
 
     public CategoriaDTO cadastrar(CategoriaDTO categoriaDTO) {
+        categoriaJaCadastrada(categoriaDTO);
         Categoria categoria = new Categoria();
         categoria.setDescricao(categoriaDTO.getDescricao());
         categoriaRepository.save(categoria);
-        return categoriaDTO;
+        CategoriaDTO categoriaCadastrada = modelMapper.map(categoria, CategoriaDTO.class);
+        return categoriaCadastrada;
     }
 
     public CategoriaDTO editar(Long id, CategoriaDTO categoriaDTO) {
@@ -40,5 +46,12 @@ public class CategoriaService {
     public void deletar(Long id) {
         Categoria categoria = getById(id);
         categoriaRepository.delete(categoria);
+    }
+
+    private void categoriaJaCadastrada(CategoriaDTO categoriaDTO) {
+        Optional<Categoria> categoria = categoriaRepository.findByDescricao(categoriaDTO.getDescricao());
+        if (categoria.isPresent()) {
+            throw new RuntimeException("Categoria "+ categoriaDTO.getDescricao() + " Já cadastrada");
+        }
     }
 }
